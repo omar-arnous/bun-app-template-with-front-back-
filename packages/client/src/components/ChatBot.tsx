7ae8643f31c01c6110842652f4bda59710badb2e
omar arnous <omar99.arnous@gmail.com>
2025-09-03 14:25:26 +0300

    Refactor: structure the backend to MVC  layout

---
+++ packages/client/src/components/ChatBot.tsx
@@ -0,0 +1,5 @@
+const ChatBot = () => {
+  return <div>ChatBot</div>;
+};
+
+export default ChatBot;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,65 +1,14 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
-import z from 'zod';
+import chatRouter from './routes/chat.routes';
 
 dotenv.config();
 
-const client = new OpenAI({
-  apiKey: process.env.OPEN_API_KEY,
-});
-
 const app = express();
 app.use(express.json());
+app.use(chatRouter);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World!' });
-});
-
-// let lastResponseId: string | null = null;
-const conversations = new Map<string, string>();
-
-const chatSchema = z.object({
-  prompt: z
-    .string()
-    .trim()
-    .min(1, 'Prompt is required.')
-    .max(1000, 'Prompt is too long (max 1000 characters)'),
-  conversationId: z.string().uuid(),
-});
-
-app.post('/api/chat', async (req: Request, res: Response) => {
-  const parseResult = chatSchema.safeParse(req.body);
-
-  if (!parseResult.success)
-    return res.status(400).json(parseResult.error.format());
-
-  try {
-    const { prompt, conversationId } = req.body;
-
-    const response = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input: prompt,
-      temperature: 0.2,
-      max_output_tokens: 100,
-      // previous_response_id: lastResponseId,
-      previous_response_id: conversations.get(conversationId),
-    });
-
-    // lastResponseId = response.id;
-    conversations.set(conversationId, response.id);
-
-    res.json({ message: response.output_text });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to generate a response.' });
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
